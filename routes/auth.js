215ce6c5aa80822570ef418dccb2261221285aad
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-23 19:35:41 -0400

    adding schema for validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const {registervalidation, loginValidation} = require('../validation');
+const {registerValidation} = require('../validation');
 
 
 router.post('/register', async (req, res) => {
 
     //Lets validate the data before we make a user
-    const {error} = registervalidation(req.body);
+    const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     const user = new User({
--- validation.js
+++ validation.js
@@ -1,24 +1,24 @@
 //Validation
-const joi = require('@hapi/joi');
+const Joi = require('@hapi/joi');
 
 //Regiser validation
 const registerValidation = (data) => {
     const schema = {
-        name: joi.string().min(6).required(),
-        email: joi.string().min(6).required().email(),
-        password: joi.string().min(6).required()
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
     };
 
-    return joi.validate(data, schema);
+    return Joi.validate(data, schema);
 };
 
 const loginValidation = (data) => {
     const schema = {
-        email: joi.string().min(6).required().email(),
-        password: joi.string().min(6).required()
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
     };
 
-    return joi.validate(data, schema);
+    return Joi.validate(data, schema);
 };
 
 module.exports.registerValidation = registerValidation;
