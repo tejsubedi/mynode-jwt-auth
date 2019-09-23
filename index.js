8e36af1b9e3fbfe315ee7684d904e3683b07a762
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-22 20:44:17 -0400

    added router

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
-
 const app = express();
+
+//Import routes
+const authRoute = require('./routes/auth');
+
+
+
+
+//Routes middleware
+app.use('/api/user', authRoute);
+
+
 
 app.listen(8080, () => console.log(`Server started in port 8080.`));
---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.post('/register',(req, res) => {
+    res.send('Registered');
+})
+
+
+module.exports = router;
